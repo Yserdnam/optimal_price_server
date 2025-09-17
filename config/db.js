536d3bea389a51e1234fe87e29cbef8bbe0847d4
RASAMOELINA Andriandraina Mandresy <89485951+Yserdnam@users.noreply.github.com>
2025-09-17 08:24:11 +0300

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 require("dotenv").config(); // <== Ajout
 
+console.log('MONGO_URI:', process.env.MONGO_URI);
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI, {
